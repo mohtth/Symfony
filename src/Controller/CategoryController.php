d67310d30dee9b0ca394981fd7bf5a7735011538
Tth <mtoutouhi@gmail.com>
2019-06-04 17:06:41 +0200

    Quete 10 fini

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,18 +2,43 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Category;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Form\CategorieType;
+
 
 class CategoryController extends AbstractController
 {
+
+
     /**
      * @Route("/category", name="category")
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @return Response
      */
-    public function index()
+    public function add(Request $request, EntityManagerInterface $entityManager)
     {
+        $category = new Category();
+        $form = $this->createForm(CategorieType::class, $category);
+        $form->handleRequest($request);
+dump($category);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+        }
+
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
-        ]);
+                'monForm' => $form->createView(),
+            ]);
+
     }
 }
